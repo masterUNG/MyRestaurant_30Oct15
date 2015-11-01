6cb501d6307977e424c0803ad4e36adee5cbc79f
masterUNG <phrombutr@gmail.com>
2015-11-01 12:27:56 +0700

    Before Lunch

--- app/src/main/java/appewtc/masterung/myrestaurant/OrderActivity.java
+++ app/src/main/java/appewtc/masterung/myrestaurant/OrderActivity.java
@@ -2,12 +2,31 @@ package appewtc.masterung.myrestaurant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class OrderActivity extends AppCompatActivity {
+
+    //Explicit
+    private TextView officerTextView;
+    private Spinner deskSpinner;
+    private ListView foodListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+        //Bind Widget
+        bindWidget();
+
+    }   // Main Method
+
+    private void bindWidget() {
+        officerTextView = (TextView) findViewById(R.id.textView);
+        deskSpinner = (Spinner) findViewById(R.id.spinner);
+        foodListView = (ListView) findViewById(R.id.listView);
     }
-}
+
+}   // Main Class
