57ab6c90b2f397810c52cc2d1df3d10dd3712d35
masterUNG <phrombutr@gmail.com>
2015-11-01 13:06:41 +0700

    Spinner

--- app/src/main/java/appewtc/masterung/myrestaurant/OrderActivity.java
+++ app/src/main/java/appewtc/masterung/myrestaurant/OrderActivity.java
@@ -2,6 +2,7 @@ package appewtc.masterung.myrestaurant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -12,6 +13,7 @@ public class OrderActivity extends AppCompatActivity {
     private TextView officerTextView;
     private Spinner deskSpinner;
     private ListView foodListView;
+    private String officerString, deskString, foodString, itemString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +23,41 @@ public class OrderActivity extends AppCompatActivity {
         //Bind Widget
         bindWidget();
 
+        //Show Officer
+        showOfficer();
+
+        //Show Spinner
+        showSpinner();
+
     }   // Main Method
+
+    private void showSpinner() {
+
+        String[] strDesk = new String[10];
+        strDesk[0] = "A1";
+        strDesk[1] = "A2";
+        strDesk[2] = "A3";
+        strDesk[3] = "A4";
+        strDesk[4] = "A5";
+        strDesk[5] = "B1";
+        strDesk[6] = "B2";
+        strDesk[7] = "B3";
+        strDesk[8] = "B4";
+        strDesk[9] = "B5";
+
+        ArrayAdapter<String> deskAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                strDesk);
+        deskSpinner.setAdapter(deskAdapter);
+
+
+
+    }
+
+    private void showOfficer() {
+        officerString = getIntent().getStringExtra("Name");
+        officerTextView.setText(officerString);
+    }
 
     private void bindWidget() {
         officerTextView = (TextView) findViewById(R.id.textView);
